70ddffd6705eb71fec27fbe1b4d158a6544cd962
nethranjalijayasanki <ssunflower620@gmail.com>
2025-01-26 08:25:03 +0530

    DbConnection renew

---
+++ src/main/java/com/example/ecommerce_web_applicationjavaee/utils/DbConnection.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce_web_applicationjavaee.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ecommerce",
+                "root",
+                "Ijse@123"
+        );
+    }
+
+    public static DbConnection getInstance() throws SQLException {
+        if(dbConnection == null) {
+            dbConnection = new DbConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
